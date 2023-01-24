524c84273f3c2fe19e211cd206335e00232b0546
Darren Kim <dkim555@yahoo.com>
2023-01-24 15:53:36 -0800

    code guess counter

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let ranNum = Math.floor(Math.random() * 100);
 let message = document.getElementById('messageText');
-
+let guessCounter = 3
     
 function checkNum() {
     let userGuess = document.getElementById('numGuess').value;
     if (userGuess > ranNum) {
-        message.innerHTML = 'Your number is too high';
+        guessCounter -= 1;
+        message.innerHTML = `Your number is too high. You have ${guessCounter} guess left.`;
     }
     if (userGuess < ranNum) {
-        message.innerHTML = 'Your number is too low';
+        guessCounter -= 1;
+        message.innerHTML = `Your number is too low. You have ${guessCounter} guess left.`;
     }
-    if (userGuess == ranNum) {
+    if (userGuess == ranNum && guessCounter <= 3) {
         message.innerHTML = 'That is correct!'
     }
     if (isNaN(userGuess) || userGuess ==='' || userGuess === null) {
         message.innerHTML = 'Not a number'
+    }
+    if (guessCounter === 0) {
+        message.innerHTML = `You have no more guess left, loser. The number was ${ranNum}`
     }
 }
