dc6b1f5e110ecb758420ecbe8899a330acbe8972
Darren Kim <dkim555@yahoo.com>
2023-01-24 11:16:35 -0800

    write conditionals

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let ranNum = Math.floor(Math.random() * 100);
-let userGuess = document.getElementById('submitButton');
+let submitBtn = document.getElementById('submitButton');
 let reset = document.getElementById('resetButton');
 let message = document.getElementById('messageText');
+
+function checkNum() {
+    let userGuess = document.getElementById('numGuess').value;
+    if (userGuess > ranNum) {
+        message.innerHTML = 'Your number is too high';
+    }
+    if (userGuess < ranNum) {
+        message.innerHTML = 'Your number is too low';
+    }
+    if (userGuess === ranNum) {
+        message.innerHTML = 'That is correct!'
+    }
+}
