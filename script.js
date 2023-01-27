44b6a2866469e96967b5bbdb622917dfdc2fae9c
Darren Kim <dkim555@yahoo.com>
2023-01-26 21:35:07 -0800

    fix typo

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function checkNum() {
         message.innerHTML = 'Not a number'
     }
     if (userGuess > ranNum) {
-        message.innerHTML = `Your number is too high. You have ${guessCounter} guess left.`;
+        message.innerHTML = `Your number is too high. You have ${guessCounter} guesses left.`;
     }
     if (userGuess < ranNum) {
-        message.innerHTML = `Your number is too low. You have ${guessCounter} guess left.`;
+        message.innerHTML = `Your number is too low. You have ${guessCounter} guesses left.`;
     }
     if (userGuess === ranNum && guessCounter <= 3) {
         message.innerHTML = 'That is correct!'
     }
     if (guessCounter === 0) {
-        message.innerHTML = `You have no more guess left, loser. The number was ${ranNum}`;
+        message.innerHTML = `You have no more guesses left, loser. The number was ${ranNum}`;
     }
     if (guessCounter < 0) {
         resetGame();
